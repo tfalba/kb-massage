77e97e669e54cd04c34d99ddef342b6a2ed3be0e
Tracy Falba <tfalba@mac.com>
2025-12-04 01:10:40 -0500

    nav height fix

--- src/components/SimpleModal/SimpleModal.tsx
+++ src/components/SimpleModal/SimpleModal.tsx
@@ -16,7 +16,7 @@ export default function SimpleModal({
       <button
         onClick={openModal}
         className="bg-brand-sage text-white"
-        style={{ height: "calc(max(12vw, 14vh, 105px))" }}
+        style={{ height: "calc(max(12vw, 14vh, 110px))" }}
       >
         <span className="grid w-[max(5vw,5vh)] place-content-center font-montserrat text-[clamp(1rem,1.2vw,1.6rem)] font-semibold tracking-widest text-white [writing-mode:vertical-rl]">
           Book Now
